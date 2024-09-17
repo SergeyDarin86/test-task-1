9aeabba896b8781727e0fe17528c7f17dbeb9bc9
sergei_darin <swd86@mail.ru>
2024-09-17 15:45:47 +0400

    Initial commit

---
+++ src/main/java/ru/darin/testTask/model/StringModel.java
@@ -0,0 +1,38 @@
+package ru.darin.testTask.model;
+//
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.NotEmpty;
+//import jakarta.validation.constraints.Pattern;
+//import jakarta.validation.constraints.Size;
+//
+//@Entity
+//@Table(name = "string_model")
+public class StringModel {
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "str_id")
+//    private Integer id;
+//
+//    @Column(name = "str")
+//    @NotEmpty(message = "Строка не должна быть пустой")
+//    @Size(min = 1, message = "Строка должна содержать минимум 1 символ")
+//    @Pattern(regexp = "^[а-яА-ЯёЁa-zA-Z0-9]+$", message = "Неверный формат ввода данных: Вы должны ввести набор из цифр, букв (латиница, кириллица)")
+//    private String str;
+//
+//    public Integer getId() {
+//        return id;
+//    }
+//
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//
+//    public String getStr() {
+//        return str;
+//    }
+//
+//    public void setStr(String str) {
+//        this.str = str;
+//    }
+}
